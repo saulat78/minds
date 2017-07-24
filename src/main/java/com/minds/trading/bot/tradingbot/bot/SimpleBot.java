094a9b9d2091cdfd1810b174aab5798c90e784dd
Syed Saulat Rizvi <saulat78@yahoo.com>
2017-07-24 00:18:46 -0500

    added buying price limit and change percentage

--- src/main/java/com/minds/trading/bot/tradingbot/bot/SimpleBot.java
+++ src/main/java/com/minds/trading/bot/tradingbot/bot/SimpleBot.java
@@ -16,10 +16,17 @@ public class SimpleBot implements Bot
 	private MindsMarketDatastore datastore = MindsMarketDatastoreImpl.getInstance();
 	private DecimalFormat df = new DecimalFormat("#.####");
 	private final static Logger log = LoggerFactory.getLogger(SimpleBot.class);
-
-	public SimpleBot(String currencyPair)
+	private double buyPriceLimit = -1;
+	private int perTransactionBuyingQty = -1;
+	private double feePercent = 0.50; //includes buying and selling
+	private int totalBuyingQty =-1;
+	
+	public SimpleBot(String currencyPair,double buyPriceLimit, int perTransactionBuyingQty, int totalBuyingQty )
 	{
 		this.currencyPair = currencyPair;
+		this.buyPriceLimit = buyPriceLimit;
+		this.perTransactionBuyingQty = perTransactionBuyingQty;
+		this.totalBuyingQty = totalBuyingQty;
 		 df.setRoundingMode(RoundingMode.CEILING);
 
 	}
@@ -37,20 +44,16 @@ public class SimpleBot implements Bot
 	
 		try
 		{
-			 Thread.sleep(2000);
-			 log.info(currencyPair +" Current Price : " + datastore.getCurrentPrice(currencyPair));
-			
-			 while(true)
-			 {
+			 Thread.sleep(3000);
 				 double prevPrice = datastore.getCurrentPrice(currencyPair).doubleValue();
 				 Thread.sleep(1000);
 				 double currPrice =  datastore.getCurrentPrice(currencyPair).doubleValue();
 				 if(prevPrice != currPrice)
 				 {
-					 double percChange = ((currPrice - prevPrice)/prevPrice)*100;
-					 log.info(currencyPair+":" + currPrice + "  Change=" + df.format(percChange) + "%" );
+					 double percChangePrevPrice = ((currPrice - prevPrice)/prevPrice)*100;
+					 double percChangeBuyingPrice = ((currPrice - this.buyPriceLimit)/this.buyPriceLimit)*100;
+					 log.info(currencyPair+":" + currPrice + " BuyingPriceChange " + percChangeBuyingPrice + "%  Change=" + df.format(percChangePrevPrice) + "%" );
 				 }
-			 }
 			}
 		 catch(Exception e)
 		 {
@@ -59,8 +62,24 @@ public class SimpleBot implements Bot
 		return true;
 	}
 
+	private void buy(double currentPrice)
+	{
+		if(currentPrice <= this.buyPriceLimit)
+		{
+			double totalbuyingPrice = this.perTransactionBuyingQty * currentPrice;
+			double minSellingPrice =  (totalbuyingPrice * this.feePercent) /100;
+			log.info("SimpleBot bought " + this.perTransactionBuyingQty + " each for " +  currentPrice + " total amt " + totalbuyingPrice + " selling price is " + minSellingPrice );
+			//TODO add all the bought coin into a list and iterate over it in the sell method to check what to sell
+		}
+	}
+	
+	private void sell()
+	{
+		
+	}
 	@Override
-	public void stop() {
+	public void stop()
+	{
 		
 		
 	}
