d6d5271b0e3903a1f14a47b703f9056ade4e212c
Syed Saulat Rizvi <saulat78@yahoo.com>
2017-07-25 01:12:00 -0500

    changed test run params

--- src/main/java/com/minds/trading/exchange/example/SimpleBotRunner.java
+++ src/main/java/com/minds/trading/exchange/example/SimpleBotRunner.java
@@ -14,7 +14,7 @@ public class SimpleBotRunner {
 	public static void main(String arg[]) throws Exception
 	{
 		List<Bot> list = new ArrayList<>();
-		SimpleBot sb = new SimpleBot(Constants.CURRENT_CURR_PAIR, 0.00000819, 1,5);
+		SimpleBot sb = new SimpleBot(Constants.CURRENT_CURR_PAIR, 0.00000819, 5 ,5);
 		BotManager bm = new BotManager();
 		list.add(sb);
 		bm.startBots(list);
